94f87ab7bb155c0bc7fbcea4312dae59cf02722e
Yaroslav Kushner <slavik023@gmail.com>
2022-10-07 22:00:24 +0200

    Changes synchronization-task1 h/w

--- syncronization-task1/src/main/java/Main.java
+++ syncronization-task1/src/main/java/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static final int BUYERS_IN_HOUSE = 10;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         final Dealership dealership = new Dealership();
 
         for (int i = 1; i <= BUYERS_IN_HOUSE; i++) {
             new Thread(null, dealership::sellCar, "Покупатель " + i).start();
+            Thread.sleep(1000);
+            if (BUYERS_IN_HOUSE>3){
+                new Thread(null, dealership::receiveCar, "Производитель").start();
+            }
         }
-        new Thread(null, dealership::receiveCar, "Производитель").start();
     }
 }
